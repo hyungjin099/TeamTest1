87f86b6c32258b2a22b552b95b81c532ca6aa305
hyungjin099 <hyungjin099@gmail.com>
2023-09-27 11:12:58 +0900

    dlseprtm zjsxmfhffj tnwjd

--- src/main/java/com/green/TeamTest1/IndexController.java
+++ src/main/java/com/green/TeamTest1/IndexController.java
@@ -9,6 +9,8 @@ public class IndexController {
     //시작페이지
     @GetMapping("/")
     public String main(){
+        System.out.println("지옥에 온 것을 환영합니다~");
+        System.out.println("지옥에 온 것을 환영합니다~");
         return "index";
     }
 

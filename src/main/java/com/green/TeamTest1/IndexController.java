c232a43409fff28fe500805368fa35e59b2f62c6
hyungjin099 <hyungjin099@gmail.com>
2023-09-27 10:22:57 +0900

    프로젝트 생성

---
+++ src/main/java/com/green/TeamTest1/IndexController.java
@@ -0,0 +1,15 @@
+package com.green.TeamTest1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    //시작페이지
+    @GetMapping("/")
+    public String main(){
+        return "index";
+    }
+
+}
